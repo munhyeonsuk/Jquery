a5d1ea66dee53b97f88646b6de7ee1b1adea0314
munhyeonsuk <m01024038735@gmail.com>
2024-06-18 11:10:43 +0900

    제이쿼리비동기연습

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -11,8 +11,9 @@ $(document).ready(function(){
             data["menu"].forEach(function(ele, idx){
                 //순서대로 순회하면서 실행
                 console.log(`${idx}번째 데이터는 ${ele}이다`)
+                gnbli += `<li>${ele["category"]}</li>`
             });
-            $(".gnb").html()
+            $(".gnb").html(gnbli)
         },
         error : function(a, b, c){
             console.log(a, b, c)
