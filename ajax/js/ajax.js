359b9a61dc47539f42660dc3ac0ed7ea8332ba71
munhyeonsuk <m01024038735@gmail.com>
2024-06-18 12:12:07 +0900

    forEach 3단카테고리

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -1,22 +1,40 @@
-// 화면이 다 되면 움직인다
-$(document).ready(function(){
+$(document).ready(function(){     
     $.ajax({
         url : "/ajax/json/data.json",
         type : "GET",
         dataType : "json",
         success : function(data){
             console.log(data, typeof data);
-            //Json.parse(data) -> 자바스크립트가 쓸 수 있게 object
+            // JSON.parse(data)  -> 자바스크립트가 쓸 수 있게 object
             let gnbli = "";
+
             data["menu"].forEach(function(ele, idx){
-                //순서대로 순회하면서 실행
-                console.log(`${idx}번째 데이터는 ${ele}이다`)
-                gnbli += `<li>${ele["category"]}</li>`
+                // 순서대로 순회하면서 실행
+                console.log(`${idx}번째 데이터는 ${ele}`)
+                gnbli +=`<li>${ele["category"]}`;
+                gnbli +=`<ul>`;
+                ele["subcategories"].forEach(function(eele, iidx){
+                    gnbli +=`<li>${eele["name"]}`;
+                    gnbli +=`<ul>`;
+                    eele["items"].forEach(function(eeele, iiidx){
+                        gnbli +=`<li>${eeele}</li>`;
+                    })
+                    gnbli +=`</ul>`;
+                    gnbli +=`</li>`;
+                })
+                gnbli +=`</ul>`;
+                gnbli +=`</li>`
+                
             });
+
+
             $(".gnb").html(gnbli)
+
         },
         error : function(a, b, c){
-            console.log(a, b, c)
+           console.log(a, b, c);
         }
     })
 })
+
+
