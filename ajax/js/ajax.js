2da198f4a18a29c8e1ecd875879468ea2448ac56
munhyeonsuk <m01024038735@gmail.com>
2024-06-18 10:56:36 +0900

    ajax.js에서 console창에서 데이터타입 확인

---
+++ ajax/js/ajax.js
@@ -0,0 +1,21 @@
+// 화면이 다 되면 움직인다
+$(document).ready(function(){
+    $.ajax({
+        url : "/ajax/json/data.json",
+        type : "GET",
+        dataType : "json",
+        success : function(data){
+            console.log(data, typeof data);
+            //Json.parse(data) -> 자바스크립트가 쓸 수 있게 object
+            let gnbli = "";
+            data["menu"].forEach(function(ele, idx){
+                //순서대로 순회하면서 실행
+                console.log(`${idx}번째 데이터는 ${ele}이다`)
+            });
+            $(".gnb").html()
+        },
+        error : function(a, b, c){
+            console.log(a, b, c)
+        }
+    })
+})
